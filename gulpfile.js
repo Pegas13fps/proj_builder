1815425dc182d9579e4251a681b3ad9730cd1aab
Pegas13fps <Tymoschenkob@gmail.com>
2018-10-20 00:33:19 +0300

    Added my proj builder

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browserSync = require('browser-sync');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglifyjs');
+let cssnano = require('gulp-cssnano');
+let rename = require('gulp-rename');
+let del = require('del');
+let imagemin = require('gulp-imagemin');
+let pngquant = require('imagemin-pngquant');
+let cache = require('gulp-cache');
+let autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+    return gulp.src('app/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('app/css/libs.css')
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    }); 
+});
+// Удаляем dist
+gulp.task('clean', function() {
+    return del.sync('dist');
+});
+// Чистим все файлы которые лежат не по феншую =)
+gulp.task('clear', function() {
+    return cache.clearAll();
+});
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*')
+    .pipe(cache(imagemin({
+        interlaced: true,
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        une: [pngquant()]
+    })))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+    gulp.watch('app/sass/**/*.sass', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+
+    let buildCss = gulp.src([
+        'app/css/main.css',
+        'app/css/libs.min.css',
+    ])
+    .pipe(gulp.dest('dist/css'));
+    
+    let buildFonts = gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+
+    let buildJs = gulp.src('app/js/**/*')
+    .pipe(gulp.dest('dist/js'));
+
+    let buildHtml = gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
+});
