eeaf7cc1d4ab5b91317eb1869d7563f7c01d66a2
Pegas13fps <Tymoschenkob@gmail.com>
2018-10-27 00:51:11 +0300

    Added line effects

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,27 @@
-$(function() {
-    // $('body').fadeOut();
-});
+// line.onclick = function() {
+
+//     this.style.height = 200 + 'px';
+//   }
+
+    // let a = document.querySelector("#line").style.height;
+    // console.log(a);
+
+window.onscroll = function() {
+	let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+	if (scrolled > "900" ) {
+        document.querySelector('.l1').style.height="130px";
+        document.querySelector('.l2').style.height="130px";
+		document.querySelector('.l3').style.height="80px";
+        document.querySelector('.l4').style.height="130px";
+		document.querySelector('.l5').style.height="132px";
+		document.querySelector('.l6').style.height="90px";        
+	}
+	else {
+        document.querySelector('.l1').style.height="0px";
+        document.querySelector('.l2').style.height="0px";
+		document.querySelector('.l3').style.height="0px";
+        document.querySelector('.l4').style.height="0px";
+		document.querySelector('.l5').style.height="0px";
+		document.querySelector('.l6').style.height="0px";
+	}
+}
