7416112af826b33cd22a75a2f8b11f11a6525e8d
Pegas13fps <Tymoschenkob@gmail.com>
2018-10-27 20:18:35 +0300

    Text and corrected lines

--- app/js/common.js
+++ app/js/common.js
@@ -9,19 +9,19 @@
 window.onscroll = function() {
 	let scrolled = window.pageYOffset || document.documentElement.scrollTop;
 	if (scrolled > "900" ) {
-        document.querySelector('.l1').style.height="130px";
-        document.querySelector('.l2').style.height="130px";
-		document.querySelector('.l3').style.height="80px";
-        document.querySelector('.l4').style.height="130px";
-		document.querySelector('.l5').style.height="132px";
-		document.querySelector('.l6').style.height="90px";        
+        document.querySelector('.line1').style.height="130px";
+        document.querySelector('.line2').style.height="130px";
+		document.querySelector('.line3').style.height="80px";
+        document.querySelector('.line4').style.height="130px";
+		document.querySelector('.line5').style.height="132px";
+		document.querySelector('.line6').style.height="90px";        
 	}
 	else {
-        document.querySelector('.l1').style.height="0px";
-        document.querySelector('.l2').style.height="0px";
-		document.querySelector('.l3').style.height="0px";
-        document.querySelector('.l4').style.height="0px";
-		document.querySelector('.l5').style.height="0px";
-		document.querySelector('.l6').style.height="0px";
+        document.querySelector('.line1').style.height="0px";
+        document.querySelector('.line2').style.height="0px";
+		document.querySelector('.line3').style.height="0px";
+        document.querySelector('.line4').style.height="0px";
+		document.querySelector('.line5').style.height="0px";
+		document.querySelector('.line6').style.height="0px";
 	}
 }
